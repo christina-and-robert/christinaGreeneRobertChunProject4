8d09fbae6810d03d0d05f030bc06ce19a7ba544b
Christina Greene <christina.greene@rogers.com>
2019-05-23 15:10:47 -0400

    html set up and all partials

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,23 @@ slotsApp = {};
 
 
 var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=Mississauga&limit=25",
-    "method": "GET",
-    "headers": {
-        "Authorization": "Bearer GAMYzGkShejSRIXgbB988do_OXKkIjVLpZ4Zeb9VgPrRdZBGCY44fbsYsjzoh4IWsX1O8KLwLfbWu6o4BYy29jZp3nXs1SaUNhxHA2hCBWbJ6fJR0qhcjZ2ok27kXHYx",
-        "User-Agent": "PostmanRuntime/7.13.0",
-        "Accept": "*/*",
-        "Cache-Control": "no-cache",
-        "Postman-Token": "c1030027-5b1f-4caf-969d-84ec64666426,f9c8b243-f557-4cfc-9286-759a818cd6a1",
-        // "Host": "api.yelp.com",
-        // "accept-encoding": "gzip, deflate",
-        // "Connection": "keep-alive",
-        "cache-control": "no-cache"
-    }
+   "async": true,
+   "crossDomain": true,
+   "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=Mississauga&limit=25",
+   "method": "GET",
+   "headers": {
+      "Authorization": "Bearer GAMYzGkShejSRIXgbB988do_OXKkIjVLpZ4Zeb9VgPrRdZBGCY44fbsYsjzoh4IWsX1O8KLwLfbWu6o4BYy29jZp3nXs1SaUNhxHA2hCBWbJ6fJR0qhcjZ2ok27kXHYx",
+      "User-Agent": "PostmanRuntime/7.13.0",
+      "Accept": "*/*",
+      "Cache-Control": "no-cache",
+      "Postman-Token": "c1030027-5b1f-4caf-969d-84ec64666426,f9c8b243-f557-4cfc-9286-759a818cd6a1",
+      // "Host": "api.yelp.com",
+      // "accept-encoding": "gzip, deflate",
+      // "Connection": "keep-alive",
+      "cache-control": "no-cache"
+   }
 }
 
 $.ajax(settings).done(function (response) {
-    console.log(response);
+   console.log(response);
 });
